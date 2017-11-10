719035740d1b2a3fd7a4ddc3d4d853cf0db054e4
jackLei <threetowns@163.com>
2017-11-10 18:41:03 +0800

    Add: include iview components library.

--- src/util/fetch.js
+++ src/util/fetch.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { Message } from 'iview';
+
 
 // baseURL, 将自动加在 `url` 前面，除非 `url` 是一个绝对 URL.
 if (process.env.NODE_ENV == 'development') {
@@ -19,33 +21,32 @@ axios.interceptors.request.use(config => {
     console.log('正在发送请求。。。')
     return config;
 },error => {
-    console.log('发送请求失败：'+error); // for debug
+    Message.error(error)
     return Promise.reject(error);
 });
 
 // respone拦截器
-axios.interceptors.response.use(
-  response => {
+axios.interceptors.response.use(response => {
     /**
      * 下面的注释为通过response自定义code来标示请求状态，当code返回如下情况为权限有问题，登出并返回到登录页
      * 如通过xmlhttprequest 状态码标识 逻辑可写在下面error中
      */
     const res = response.data;
     if (response.status === 401 || res.status === 40101) {
-      console.log('response:'+'401， 没有权限')
+      Message.error('401， 没有权限')
       return Promise.reject('error');
     }
     if (res.status === 40301) {
-      console.log('response:403, 当前用户无相关操作权限！')
+      Message.error('403, 当前用户无相关操作权限！')
       return Promise.reject('error');
     }
     if (response.status !== 200 && res.status !== 200) {
-      console.log('其它非200错误：'+res.message)
+      Message.error(res.message)
     } else {
       return response.data;
     }
   },error => {
-    console.log('err：' + error); // for debug
+    Message.error(error)
     return Promise.reject(error);
   }
 );
