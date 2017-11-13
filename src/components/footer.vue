88daed90252d098b6d05fd459853257f01e3471f
jackLei <threetowns@163.com>
2017-11-13 19:30:13 +0800

    Add: footer components.

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footer">
+    <img src="~assets/images/logoIcon.svg" alt="杭州钱塘大数据交易中心有限公司">
+    <p>杭州钱塘大数据交易中心有限公司 QIANTANG BIG DATA EXCHANGE CENTER CO., LTD.</p>
+  </div>
+</template>
+
+<style lang="less" scoped>
+  .footer{
+    text-align: center;
+    margin-top: 52px;
+    img{
+      height: 52px;
+    }
+    p{
+      font-size: 12px;
+      text-align: center;
+      color: #56607c;
+    }
+  }
+</style>
