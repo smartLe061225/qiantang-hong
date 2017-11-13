1d9e7d0d949facd265b0220b5c231b70c351272e
jackLei <threetowns@163.com>
2017-11-13 11:15:03 +0800

    [router] Add: config vue router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,62 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const admin = r => require.ensure([], () => r(require('../views/admin/admin')), 'admin')
+const login = r => require.ensure([], () => r(require('../views/login/login')), 'login')
+const join = r => require.ensure([], () => r(require('../views/join/join')), 'join')
+const notFound = r => require.ensure([], () => r(require('components/error/404')), 'notFound')
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld
+const routes = [
+  {
+    path: '/',
+    name: 'admin',
+    component: admin,
+    meta: {
+      title: '管理中心'
     }
-  ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: login,
+    meta: {
+      title: '登录'
+    }
+  },
+  //注册页
+  {
+    path: '/join',
+    name: 'join',
+    component: join,
+    meta: {
+      title: '注册'
+    }
+  },
+  {
+    path: "/*",
+    name: "error",
+    component: notFound,
+    meta: {
+      title: '404'
+    }
+  }
+]
+
+
+
+const router = new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+
+  document.title = to.meta.title ? to.meta.title : '宏管理'
+  next()
+
+})
+
+export default router;
