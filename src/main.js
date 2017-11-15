8294c7ee873ab22531b5dae0a4c751029e04165c
william_mac <dongweiliang@gmail.com>
2017-11-15 11:07:39 +0800

    add vue-cookie vuex

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueCookie from 'vue-cookie';
+import Vuex from 'vuex'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
 
 Vue.use(iView);
+Vue.use(VueCookie);
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
