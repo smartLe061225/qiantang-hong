437bb245f5948e10875a55c9b0a5057e798127ec
william_mac <dongweiliang@gmail.com>
2017-11-20 10:08:01 +0800

    add 企业管理添加独立index

---
+++ src/views/company/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <bread-curm-bar></bread-curm-bar>
+    <div class="m-25 mt-65">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+<script>
+import breadCurmBar from '@/components/bread_curmb_bar'
+export default {
+  components: {
+    breadCurmBar
+  },
+  data() {
+    return {
+      bread_crumb_data: null
+    };
+  }
+};
+</script>
