868e9495089f67cbde7996855f7b958d916f480f
jackLei <threetowns@163.com>
2017-11-22 20:17:42 +0800

    [echarts] Add: getCompanyData feature.

--- src/util/echarts.js
+++ src/util/echarts.js
@@ -1,4 +1,5 @@
 import { ajaxPostAnalysisPublicData } from "src/apis/analysis";
+import { ajax_get_company_selectbox } from "src/apis/company";
 
 const echartsConfig = {
   color: ['#00c35c','#0e7ccf','#ffd900','#b7a3df','#e55e7e','#ffc0c0','#ff9a23','#f1c2e6','#f8c66e','#26a9e6','#8be0db','#ff6b08'],
@@ -25,6 +26,16 @@ const echartsConfig = {
       })
     })
     return results;
+  },
+  getCompanyData(){
+    let result = [];
+    ajax_get_company_selectbox().then(rs => {
+      let data = rs.data;
+      data.forEach(function(v) {
+        result.push({'value':v.id, 'label':v.name})
+      })
+    })
+    return result;
   }
 }
 
