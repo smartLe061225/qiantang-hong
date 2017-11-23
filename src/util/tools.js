63d761531b0606b88259ccef12ee2fb68246053e
jackLei <threetowns@163.com>
2017-11-23 18:05:50 +0800

    [tools] Add: formatPieSeriesData feature.

--- src/util/tools.js
+++ src/util/tools.js
@@ -27,6 +27,30 @@ export const formatBarSeriesData = (resourceArr, legendDataArr) => {
 }
 
 /**
+ * 方法说明： 把数据处理成 echarts-柱状图 所需的数组格式
+ * @method formatBarSeriesData
+ * @param {array}   resourceArr,服务器返回数据结果
+ * @param {array}   legendDataArr, 指标，格式如：['营业收入','营业利润','净利润']
+ * @return {array}
+ */
+export const formatPieSeriesData = (resourceArr, legendDataArr) => {
+  let itemsArrData = resourceArr.data
+  let result = [];
+  for (var i = 0; i < legendDataArr.length; i++) {
+    if (itemsArrData.length>0) {
+      for (let j = 0; j < itemsArrData.length; j++) {
+        if(itemsArrData[j].index_name == legendDataArr[i]){
+          result.push({name: legendDataArr[i], value: itemsArrData[j].total})
+        }
+      }
+    }else{
+      result.push('')
+    }
+  }
+  return result;
+}
+
+/**
  * 方法说明： 通过 公司ID(companyIds) 返回 对应公司名(result)
  * @method getCompanyName
  * @param {array}   companyIds, 公司ID
