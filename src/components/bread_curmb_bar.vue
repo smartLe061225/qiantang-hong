ce20dfb21d4b71de7e399ee005ecf0a3f8cfbf66
jackLei <threetowns@163.com>
2017-11-25 21:09:20 +0800

    [router] Add: list router.

--- src/components/bread_curmb_bar.vue
+++ src/components/bread_curmb_bar.vue
@@ -19,12 +19,43 @@ export default {
           return item.meta.show_in_bread_curmb_bar == true
         });
       const first = matched[0];
+
       if (first && (first.name !== "dashboard" || first.path !== "/dashboard")) {
         matched = [
           { name: "dashboard", path: "/dashboard", meta: { title: "首页" } }
         ].concat(matched);
       }
+
+      if (first && (first.path == '/analysis/list/:id' )) {
+        let url = window.location.href;
+        let id = Number(url.substring(url.lastIndexOf('/')+1,url.length))
+        switch(id){
+          case 1:
+            matched = [
+              { name: "dashboard", path: "/dashboard", meta: { title: "首页" } },
+              { name: "profits", path: "/analysis/profits", meta: { title: "利润分析" } },
+              { name: "list", path: "javascript:;", meta: { title: "导入列表" } }
+            ];
+            break;
+          case 2:
+            matched = [
+              { name: "dashboard", path: "/dashboard", meta: { title: "首页" } },
+              { name: "assets", path: "/analysis/assets", meta: { title: "资产分析" } },
+              { name: "list", path: "javascript:;", meta: { title: "导入列表" } }
+            ];
+            break;
+          case 3:
+            matched = [
+              { name: "dashboard", path: "/dashboard", meta: { title: "首页" } },
+              { name: "cash", path: "/analysis/cash", meta: { title: "现金分析" } },
+              { name: "list", path: "javascript:;", meta: { title: "导入列表" } }
+            ];
+            break;
+        }
+      }
+
       this.bread_crumb_data = matched;
+
     }
   },
   watch: {
