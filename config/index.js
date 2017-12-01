5ba67c9c899b3583a09feef174ba7f615f7184da
william_mac <dongweiliang@gmail.com>
2017-12-01 15:34:13 +0800

    add 添加ie8浏览器判断，显示升级浏览器提示

--- config/index.js
+++ config/index.js
@@ -34,8 +34,8 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        // target: 'http://192.168.31.8:8762',
-        target: 'http://192.168.36.94:8762', // 测试
+        target: 'http://192.168.31.8:8762',
+        // target: 'http://192.168.36.94:8762', // 测试
         // target: 'http://192.168.31.45:8762',
         pathRewrite: {
           '^/api': '/api'
